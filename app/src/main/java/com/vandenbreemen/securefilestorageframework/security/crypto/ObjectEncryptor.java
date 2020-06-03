23ed13ce22c0ae90009cf73c298c28745016923d
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-06-03 18:21:03 -0400

    Add crypto types and also serialization/deserialization infrastructure

---
+++ app/src/main/java/com/vandenbreemen/securefilestorageframework/security/crypto/ObjectEncryptor.java
@@ -0,0 +1,31 @@
+package com.vandenbreemen.securefilestorageframework.security.crypto;
+
+import java.io.Serializable;
+
+/**
+ * <h2>Intro</h2>
+ * <p>Service that can encrypt/decrypt entire serializables in Java
+ * <h2>Other Details</h2>
+ *
+ * @author kevin
+ */
+public interface ObjectEncryptor {
+    /**
+     * Encrypt object using password
+     *
+     * @param password
+     * @param object
+     * @return Bytes representing encrypted serialized form of the object
+     */
+    byte[] encryptObject(SecureString password,
+                         Serializable object);
+
+    /**
+     * Decrypt the bytes and deserialize them into an object
+     *
+     * @param ciphertext
+     * @param password
+     * @return
+     */
+    Object decryptObject(byte[] ciphertext, SecureString password);
+}
