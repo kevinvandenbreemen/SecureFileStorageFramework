69d4bf9e9dc2cbde08b066d8d774e62eeae4de7b
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-06-04 07:46:44 -0400

    Pull in DualLayer crypto

---
+++ app/src/test/java/com/vandenbreemen/securefilestorageframework/security/crypto/DualLayerEncryptionServiceTest.java
@@ -0,0 +1,23 @@
+package com.vandenbreemen.securefilestorageframework.security.crypto;
+
+import com.vandenbreemen.securefilestorageframework.security.BytesToBits;
+
+import org.junit.Test;
+
+/**
+ * @author kevin
+ */
+public class DualLayerEncryptionServiceTest {
+
+    @Test
+    public void testDisplayBytesInDerivedKey() {
+
+        SecureString key = SecureString.fromPassword("test");
+        KeySet derived = DualLayerEncryptionService.generateKeys(key);
+
+        System.out.println("key1:  " + BytesToBits.toByteString(derived.getKey(KeySet.KEYNUM.Key1).copyBytes()));
+        System.out.println("key2:  " + BytesToBits.toByteString(derived.getKey(KeySet.KEYNUM.Key2).copyBytes()));
+
+    }
+
+}
